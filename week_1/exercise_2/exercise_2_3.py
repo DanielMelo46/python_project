f245201295e86ad7608c6e2b621d040a9ffc18b9
DanielMelo46 <daniel.melo.casstiblanco@gmail.com>
2025-08-31 22:34:28 -0500

    Testing the function

--- week_1/exercise_2/exercise_2_3.py
+++ week_1/exercise_2/exercise_2_3.py
@@ -17,11 +17,9 @@ def transform_to_dict_3(text : str = "") -> dict:
     return result
 
 # messy data string
-data = "product_name: Laptop, price = $999, brand - Dell ; product_name: Smartphone, price: $599 , brand = Samsung ; product_name - Tablet , price=299,brand : Apple"
+data3 = "product_name: Laptop, price = $999, brand - Dell ; product_name: Smartphone, price: $599 , brand = Samsung ; product_name - Tablet , price=299,brand : Apple"
 
-# expected output
-[
-    {'product_name': 'Laptop', 'price': '$999', 'brand': 'Dell'},
-    {'product_name': 'Smartphone', 'price': '$599', 'brand': 'Samsung'},
-    {'product_name': 'Tablet', 'price': '299', 'brand': 'Apple'}
-]
+print('Question 3')
+data3 = "product_name: Laptop, price = $999, brand - Dell ; product_name: Smartphone, price: $599 , brand = Samsung ; product_name - Tablet , price=299,brand : Apple"
+print(f'Before: {data3}')
+print(f'After: {transform_to_dict_3(data3)}')
