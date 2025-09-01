a51439579346dbad26915c2a5b63a04794f0e4ce
DanielMelo46 <daniel.melo.casstiblanco@gmail.com>
2025-08-31 22:58:44 -0500

    Adding the original file and the task

---
+++ week_1/exercise_3/exercise_3_2.py
@@ -0,0 +1,6 @@
+"""You are given a list of numbers. 
+Write a Python program that loops through the list and prints whether each number is even or odd."""
+# Your solution for Question 2
+numbers = [10, 21, 32, 43, 54]
+
+# Write your loop here
