dae452ac7067415f150ea13ef77418353bdf1300
DanielMelo46 <daniel.melo.casstiblanco@gmail.com>
2025-08-31 20:48:27 -0500

    Using created functions and testi

--- week_1/exercise_1_1.py
+++ week_1/exercise_1_1.py
@@ -8,22 +8,13 @@ def area_circle(radius : float = 0.0) -> float:
     return 3.14 * radius ** 2
 
 # Calculating area of rectangles
-length1 = 10
-width1 = 5
-area_rectangle1 = length1 * width1
+# Note the use of dictionaries to store rectangle properties
+rectangle1 = {'length': 10, 'width': 5}
+rectangle2 = {'length': 20, 'width': 10}
+circle1 = 5.0 # radius
+circle2 = 10.0 # radius
 
-length2 = 15
-width2 = 7
-area_rectangle2 = length2 * width2
-
-# Calculating area of circles
-radius1 = 4
-area_circle1 = 3.14 * (radius1 ** 2)
-
-radius2 = 6
-area_circle2 = 3.14 * (radius2 ** 2)
-
-print("Area of first rectangle:", area_rectangle1)
-print("Area of second rectangle:", area_rectangle2)
-print("Area of first circle:", area_circle1)
-print("Area of second circle:", area_circle2)
+print(f"Area of rectangle 1: {area_rectangle(rectangle1['length'], rectangle1['width'])}")
+print(f"Area of rectangle 2: {area_rectangle(rectangle2['length'], rectangle2['width'])}")
+print(f"Area of circle 1: {area_circle(circle1)}")
+print(f"Area of circle 2: {area_circle(circle2)}")
