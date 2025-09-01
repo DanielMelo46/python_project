4bb830624d9e14f4e77f0316733ab8db95a7e139
DanielMelo46 <daniel.melo.casstiblanco@gmail.com>
2025-08-31 22:55:53 -0500

    Adding original file

---
+++ week_1/exercise_3/exercise_3_1.py
@@ -0,0 +1,7 @@
+"""You are given a variable x with a value of 15. Write a Python program that checks if
+ x is greater than 10, equal to 10, or less than 10, and prints an appropriate message 
+ for each case. Test your program with various values to x to ensure correct results for each case."""
+# Your solution for Question 1
+x = 15
+# Write your conditions here
+
