f0307c6b7dcce2769b76e25fae1393ce4bfde753
DanielMelo46 <daniel.melo.casstiblanco@gmail.com>
2025-08-31 23:04:33 -0500

    Getting rid of the extra lsit definition

--- week_1/exercise_3/exercise_3_4.py
+++ week_1/exercise_3/exercise_3_4.py
@@ -1,7 +1,5 @@
 """Write a program that counts how many times each grade appears in the list and prints the results."""
 
-grades = [85, 92, 78, 90, 92, 85, 92]
-
 # Your solution for Question 4
 grades = [85, 92, 78, 90, 92, 85, 92]
 
