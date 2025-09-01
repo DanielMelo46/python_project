9427b4c47dec57ef4ed26aeea7d7960fe4f4673f
DanielMelo46 <daniel.melo.casstiblanco@gmail.com>
2025-08-31 23:06:42 -0500

    Checking pandas series output

--- week_1/exercise_3/exercise_3_4.py
+++ week_1/exercise_3/exercise_3_4.py
@@ -1,6 +1,9 @@
 """Write a program that counts how many times each grade appears in the list and prints the results."""
-
+import pandas as pd
 # Your solution for Question 4
 grades = [85, 92, 78, 90, 92, 85, 92]
 
 # Write your code to count occurrences
+# We are using pandas for simplicity and efficiency. Pandas series has a built-in method value_counts() that does exactly this.
+grade_series = pd.Series(grades)
+print(grade_series)
