f8a2443c5c058bbb3b579bb0a6a73c12b5464b16
DanielMelo46 <daniel.melo.casstiblanco@gmail.com>
2025-08-31 22:59:37 -0500

    Adding the loop (tested)

--- week_1/exercise_3/exercise_3_2.py
+++ week_1/exercise_3/exercise_3_2.py
@@ -4,3 +4,8 @@ Write a Python program that loops through the list and prints whether each numbe
 numbers = [10, 21, 32, 43, 54]
 
 # Write your loop here
+for number in numbers:
+    if number % 2 == 0:
+        print(f"{number} is even.")
+    else:
+        print(f"{number} is odd.")
