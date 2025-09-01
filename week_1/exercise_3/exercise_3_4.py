e0dd3da245781a03eff4ac43c5bc093770fdf59b
DanielMelo46 <daniel.melo.casstiblanco@gmail.com>
2025-08-31 23:07:36 -0500

    Using value_counts() to count the nnumber of occurrences

--- week_1/exercise_3/exercise_3_4.py
+++ week_1/exercise_3/exercise_3_4.py
@@ -6,4 +6,4 @@ grades = [85, 92, 78, 90, 92, 85, 92]
 # Write your code to count occurrences
 # We are using pandas for simplicity and efficiency. Pandas series has a built-in method value_counts() that does exactly this.
 grade_series = pd.Series(grades)
-print(grade_series)
+print(grade_series.value_counts())
