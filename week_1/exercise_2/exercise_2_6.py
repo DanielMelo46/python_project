9561ead57c6d577b0be017436e83d58f76573609
DanielMelo46 <daniel.melo.casstiblanco@gmail.com>
2025-08-31 22:45:58 -0500

    Adding transformation function

--- week_1/exercise_2/exercise_2_6.py
+++ week_1/exercise_2/exercise_2_6.py
@@ -1,3 +1,25 @@
+def transform_to_dict_6(text : str = "") -> dict:
+    """Transform a string of several key-value pairs (several char) separated by ; into a list of dictionaries."""
+    result = []
+    expected_keys = ['transaction_id', 'customer_name', 'item', 'price', 'discount']
+    chars = [':', '=', '-'] # List of possible characters that can separate key and value
+    persons = text.split(';')
+    for person in persons:
+        person_dict = {} # This dict will save each person's attributes
+        pairs = person.split(',')
+        for pair in pairs :
+            for char in chars:
+                if char in pair:
+                    k , v = pair.split(char,1)
+                    person_dict[k.strip()] = v.strip()
+                    break # Exit the loop once a match is found
+        result.append(person_dict)
+        # Ensure all expected keys are present
+        for key in expected_keys:
+            if key not in person_dict:
+                person_dict[key] = None
+    return result
+
 # messy data string
 data = "transaction_id:001, customer_name: Alice, item=Laptop, price: $999; transaction_id=002, customer_name: Bob, item - Smartphone, price= $599; transaction_id:003, item=Tablet, price:299, discount-10%; transaction_id=004, customer_name=Charlie, item: Headphones, discount=5%"
 
