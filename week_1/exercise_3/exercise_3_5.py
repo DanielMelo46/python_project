ac0d224a138bd305e8ad7d868dea90dcf3387304
DanielMelo46 <daniel.melo.casstiblanco@gmail.com>
2025-08-31 23:11:09 -0500

    Adding original code

---
+++ week_1/exercise_3/exercise_3_5.py
@@ -0,0 +1,12 @@
+"""You are given a dictionary that contains information about different countries and their populations in millions.
+Write a program that loops over the dictionary and prints the name of each country and its population. 
+If the population is over 100 million, print a message saying the country is "heavily populated."""
+
+countries = {
+    "Canada": 38,
+    "Brazil": 212,
+    "India": 1391,
+    "Germany": 83,
+    "Japan": 126
+}
+
