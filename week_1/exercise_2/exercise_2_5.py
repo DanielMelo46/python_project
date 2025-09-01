41db0e236f026f22c6fa51571608e53ca7079ded
DanielMelo46 <daniel.melo.casstiblanco@gmail.com>
2025-08-31 22:39:35 -0500

    Adding trasnformation function

--- week_1/exercise_2/exercise_2_5.py
+++ week_1/exercise_2/exercise_2_5.py
@@ -1,3 +1,16 @@
+def transform_to_dict_5(text : str = "") -> dict:
+    """Transform a string of several key-value pairs separated by a pipe into a list of dictionaries."""
+    result = []
+    entries = text.strip().split('|')
+    for entry in entries:
+        person_dict = {} # This dict will save each person's attributes
+        pairs = entry.split(',')
+        for pair in pairs :
+            k , v = pair.split(':')
+            person_dict[k.strip()] = None if v.strip == 'N/A' else v.strip()
+        result.append(person_dict)
+    return result
+
 # messy data string
 data = "device_id: 001, temp: 72, humidity: 45 | device_id: 002, temp: N/A, humidity: 50 | device_id: 003, temp: 68, humidity: N/A"
 
