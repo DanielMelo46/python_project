96d75edcf7cc15042e09bc5a4f3497c6970418ef
DanielMelo46 <daniel.melo.casstiblanco@gmail.com>
2025-08-31 22:33:44 -0500

    Adding the transformation function

--- week_1/exercise_2/exercise_2_3.py
+++ week_1/exercise_2/exercise_2_3.py
@@ -1,3 +1,21 @@
+# Question 3
+def transform_to_dict_3(text : str = "") -> dict:
+    """Transform a string of several key-value pairs (several char cases) separated by ; into a list of dictionaries."""
+    result = []
+    chars = [':', '=', '-'] # List of possible characters that can separate key and value
+    persons = text.split(';')
+    for person in persons:
+        person_dict = {} # This dict will save each person's attributes
+        pairs = person.split(',')
+        for pair in pairs :
+            for char in chars:
+                if char in pair:
+                    k , v = pair.split(char,1)
+                    person_dict[k.strip()] = v.strip()
+                    break # Exit the loop once a match is found
+        result.append(person_dict)
+    return result
+
 # messy data string
 data = "product_name: Laptop, price = $999, brand - Dell ; product_name: Smartphone, price: $599 , brand = Samsung ; product_name - Tablet , price=299,brand : Apple"
 
