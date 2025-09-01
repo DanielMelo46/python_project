2c1605213bdaef8c53e75d9d1c716f2a69cdefff
DanielMelo46 <daniel.melo.casstiblanco@gmail.com>
2025-08-31 23:00:54 -0500

    Adding the original code

---
+++ week_1/exercise_3/exercise_3_3.py
@@ -0,0 +1,9 @@
+"""You are given a sentence: "Python is fun and powerful". Write a Python program that 
+checks whether the word "fun" or "boring" is in the sentence. Print different messages 
+depending on whether the word is found or not. Be sure to test your program with different 
+sentences to ensure correct results."""
+
+# Your solution for Question 3
+sentence = 'Python is fun and powerful'
+
+# Check for words 'fun' and 'boring'
