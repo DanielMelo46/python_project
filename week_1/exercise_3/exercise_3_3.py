ebcc83ab5f668b4125da2486e4a1d1fad70b9548
DanielMelo46 <daniel.melo.casstiblanco@gmail.com>
2025-08-31 23:02:42 -0500

    Testing with word "boring"

--- week_1/exercise_3/exercise_3_3.py
+++ week_1/exercise_3/exercise_3_3.py
@@ -4,7 +4,7 @@ depending on whether the word is found or not. Be sure to test your program with
 sentences to ensure correct results."""
 
 # Your solution for Question 3
-sentence = 'Python is fun and powerful'
+sentence = 'Python is boring and powerful'
 
 # Check for words 'fun' and 'boring'
 if 'fun' in sentence:
