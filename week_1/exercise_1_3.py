9bf6681374a0c6053d3421d247a46a8ad55ed178
DanielMelo46 <daniel.melo.casstiblanco@gmail.com>
2025-08-31 22:19:04 -0500

    Using the function and refactoring the code

--- week_1/exercise_1_3.py
+++ week_1/exercise_1_3.py
@@ -6,22 +6,6 @@ def calculate_discounted_price(price : float = 0.0, discount : float = 0.0) -> f
         return price
     return price * (1 - discount )
 
-calculate_discounted_price(100, 0.10)  # Example usage
-"""# Calculate discount for customer 1
-price1 = 100
-discount1 = 0.10
-final_price1 = price1 - (price1 * discount1)
-
-# Calculate discount for customer 2
-price2 = 250
-discount2 = 0.15
-final_price2 = price2 - (price2 * discount2)
-
-# Calculate discount for customer 3
-price3 = 50
-discount3 = 0.05
-final_price3 = price3 - (price3 * discount3)
-
-print(f"Final price for customer 1: ${final_price1}")
-print(f"Final price for customer 2: ${final_price2}")
-print(f"Final price for customer 3: ${final_price3}")"""
+print(f"Final price for customer 1: ${calculate_discounted_price(100.0, 0.1)}")
+print(f"Final price for customer 2: ${calculate_discounted_price(250.0, 0.15)}")
+print(f"Final price for customer 3: ${calculate_discounted_price(50.0, 0.05)}")
