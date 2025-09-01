eedcf718d01904fe862bc078c0b6a452a5b155e8
DanielMelo46 <daniel.melo.casstiblanco@gmail.com>
2025-08-31 22:29:53 -0500

    Adding the transformation function.

--- week_1/exercise_2/exercise_2_2.py
+++ week_1/exercise_2/exercise_2_2.py
@@ -1,3 +1,19 @@
+# Question 2
+def transform_to_dict_2(text : str = "") -> dict:
+    """Transform a string of several key-value pairs separated by a pipe into a list of dictionaries.
+    Each person's attributes are separated by a comma.
+    """
+    result = []
+    persons = text.strip().split('|')
+    for person in persons:
+        person_dict = {} # This dict will save each person's attributes
+        pairs = person.split(',',1)
+        for pair in pairs :
+            k , v = pair.split(':',1)
+            person_dict[k.strip()] = v.strip()
+        result.append(person_dict)
+    return result
+
 # input
 data = "Name: Alice, Age: 30, City: Los Angeles | Name: Bob, Age: 25, City: Chicago | Name: Charlie , Age: 35, City: New York"
 
