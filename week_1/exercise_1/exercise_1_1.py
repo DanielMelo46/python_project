8c878ed1e6593f60723675a7e38b60f9505b6e1e
DanielMelo46 <daniel.melo.casstiblanco@gmail.com>
2025-09-08 15:15:21 -0500

    refactor : adding the shapes structure dict and using it to loop through the data

--- week_1/exercise_1/exercise_1_1.py
+++ week_1/exercise_1/exercise_1_1.py
@@ -7,26 +7,29 @@ def area_circle(radius : float = 0.0) -> float:
     """Calculate the area of a circle using an approximate valie of pi."""
     return 3.14 * radius ** 2
 
-# Calculating area of rectangles
-# Note the use of dictionaries to store rectangle properties
 rectangles = [
     {'name' : 'R1','length': 10, 'width': 5},
     {'name' : 'R2','length': 20, 'width': 10}
 ]
-
-for rectangle in rectangles:
-    # calculate the area
-    area = area_rectangle(rectangle["length"], rectangle["width"])
-    # print the output
-    print(f"Area of {rectangle["name"]} rectangle: {area}")
 
 circles = [
     {'name' : 'C1', 'radius' : 4},
     {'name' : 'C2', 'radius' : 6}
 ]
 
-for circle in circles :
-    #Calculation
-    area = area_circle(circle['radius'])
-    #print the output
-    print(f'Area of {circle['name']} : {area}')
+
+shapes = [
+    {'name' : 'Circle', 'instances' : circles},
+    {'name' : 'Rectangle', 'instances' : rectangles}
+]
+
+for shape in shapes:
+    shape_name = shape['name']
+    instances = shape['instances']
+    for instance in instances:
+        if shape_name == 'Circle':
+            area = area_circle(instance['radius'])
+            print(f"Area of {instance['name']} : {area}")
+        else:
+            area = area_rectangle(instance['length'], instance['width'])
+            print(f"Area of {instance['name']} : {area}")
